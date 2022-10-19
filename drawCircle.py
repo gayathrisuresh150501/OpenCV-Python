2ebf4c224253014c037c727c8d5b406bb2faba0a
Gayathri Suresh <77912752+gayathrisuresh150501@users.noreply.github.com>
2022-10-19 10:43:55 +0530

    Add files via upload

---
+++ drawCircle.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+
+blankImage = np.zeros((500, 500, 3), dtype = 'uint8')
+
+cv.circle(blankImage, (blankImage.shape[1]//2, blankImage.shape[0]//2), 50, (0, 0, 255), thickness = 2)
+
+cv.imshow('Drawing a circle', blankImage)
+
+cv.waitKey(0)
