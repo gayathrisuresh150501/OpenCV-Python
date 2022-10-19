3ecdf0c4fb0959712f5371b6ea506e1000e83101
Gayathri Suresh <77912752+gayathrisuresh150501@users.noreply.github.com>
2022-10-19 10:45:45 +0530

    Update drawCircle.py

--- drawCircle.py
+++ drawCircle.py
@@ -3,7 +3,10 @@ import numpy as np
 
 blankImage = np.zeros((500, 500, 3), dtype = 'uint8')
 
-cv.circle(blankImage, (blankImage.shape[1]//2, blankImage.shape[0]//2), 50, (0, 0, 255), thickness = 2)
+# cv.circle(blankImage, (blankImage.shape[1]//2, blankImage.shape[0]//2), 50, (0, 0, 255), thickness = 2)
+
+cv.circle(blankImage, (blankImage.shape[1]//2, blankImage.shape[0]//2), 50, (0, 0, 255), thickness = -1)
+
 
 cv.imshow('Drawing a circle', blankImage)
 
